5dff465ac1ccbf45e55e7efe2864c29a37eb644f
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2025-12-15 16:31:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    void dfs(vector<vector<int>>& adj , vector<bool>&vis , vector<int>&ans , int curr){
+        if(vis[curr])
+            return;
+        vis[curr] = true;
+        ans.push_back(curr);
+        for(auto it : adj[curr]){
+            dfs(adj , vis , ans , it);
+        }
+    }
+    vector<int> dfs(vector<vector<int>>& adj) {
+        // Code here
+        int n = adj.size();
+        vector<bool>vis(n , false);
+        vector<int>ans;
+        dfs(adj , vis , ans , 0);
+        return ans;
+    }
+};
